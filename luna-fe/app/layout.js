c20a0c9519a340700193615a89fd45c896682648
Stefania Vintilescu <110026364+stefi0703@users.noreply.github.com>
2026-07-25 13:46:17 +0300

    layout.js for google ads

--- luna-fe/app/layout.js
+++ luna-fe/app/layout.js
@@ -13,6 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18326160055"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18326160055');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
